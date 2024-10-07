6e2dc0076b86c45d0c9ba9dfc50f29e99c5f6dd5
Heki_dm <heki.se.info@gmail.com>
2024-10-07 21:35:20 +0900

    create FastestLapクラス

--- lib/src/converters/results/fastest_lap.dart
+++ lib/src/converters/results/fastest_lap.dart
@@ -1,3 +1,30 @@
+import 'package:formula1_data/formula1_data.dart';
+import 'package:formula1_data/src/converters/results/average_speed.dart';
+
 class FastestLap {
-  const FastestLap();
+  final int rank;
+  final int lap;
+  final Time time;
+  final AverageSpeed averageSpeed;
+
+  const FastestLap({
+    required this.rank,
+    required this.lap,
+    required this.time,
+    required this.averageSpeed,
+  });
+
+  factory FastestLap.fromMap(dynamic map) {
+    return FastestLap(
+      rank: int.parse(map["rank"]),
+      lap: int.parse(map["lap"]),
+      time: Time.fromMap(map["Time"]),
+      averageSpeed: AverageSpeed.fromMap(map["AverageSpeed"]),
+    );
+  }
+
+  @override
+  String toString() {
+    return "FastestLap(rank: $rank, lap: $lap, time: $time, averageSpeed: $averageSpeed)";
+  }
 }
