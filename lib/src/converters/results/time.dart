8a76d6e2a99e90c2acaed938e070d4b3597f5fef
Heki_dm <heki.se.info@gmail.com>
2024-10-07 21:35:02 +0900

    create Timeクラス

--- lib/src/converters/results/time.dart
+++ lib/src/converters/results/time.dart
@@ -1,3 +1,26 @@
 class Time {
-  const Time();
+  final int? millis;
+  final String time;
+
+  const Time({
+    this.millis,
+    required this.time,
+  });
+
+  factory Time.fromMap(dynamic map) {
+    final data = map as Map<String, dynamic>;
+    if (data.isNotEmpty && data.containsKey("millis")) {
+      return Time(
+        millis: int.parse(map["millis"]!),
+        time: map["time"]!,
+      );
+    } else {
+      return Time(time: map["time"]!);
+    }
+  }
+
+  @override
+  String toString() {
+    return "Time(millis: $millis, time: $time)";
+  }
 }
