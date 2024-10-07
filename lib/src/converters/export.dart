804945987a5b8a82d0f6e7c500aa71022447fede
Heki_dm <heki.se.info@gmail.com>
2024-10-07 21:34:10 +0900

    update import設定　更新

--- lib/src/converters/export.dart
+++ lib/src/converters/export.dart
@@ -1,6 +1,8 @@
 export './constructors/constructor.dart';
 export './drivers/driver.dart';
+export './results/average_speed.dart';
 export './results/fastest_lap.dart';
-export './results/result.dart';
 export './results/result_table.dart';
+export './results/result.dart';
 export './results/time.dart';
+export './status/status.dart';
