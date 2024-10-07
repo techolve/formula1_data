8acbcddae82e3d317a4d9f8c337465351ee349ee
Heki_dm <heki.se.info@gmail.com>
2024-10-07 21:34:31 +0900

    create AverageSpeed

---
+++ lib/src/converters/results/average_speed.dart
@@ -0,0 +1,21 @@
+class AverageSpeed {
+  final String units;
+  final double speed;
+
+  AverageSpeed({
+    required this.units,
+    required this.speed,
+  });
+
+  factory AverageSpeed.fromMap(dynamic map) {
+    return AverageSpeed(
+      units: map["units"],
+      speed: double.parse(map["speed"]),
+    );
+  }
+
+  @override
+  String toString() {
+    return "AverageSpeed(units: $units, speed: $speed)";
+  }
+}
