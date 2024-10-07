47ae4878b8cc338eb7ec18f7e866cce6178e59ae
Heki_dm <heki.se.info@gmail.com>
2024-10-07 21:36:27 +0900

    update テストケースの追加

--- test/formula1_data_test.dart
+++ test/formula1_data_test.dart
@@ -1,14 +1,36 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:formula1_data/formula1_data.dart';
+import 'package:formula1_data/src/api/drivers.dart';
+import 'package:formula1_data/src/api/results.dart';
+import 'package:formula1_data/src/api/status.dart';
 
 void main() {
-  test('get constructors data', () async {
-    List<Constructor> constructors = await getConstructors(year: 2023);
-    constructors.map(
-      (Constructor constructor) {
-        expect(constructor.constructorsId, "Red Bull");
-      },
-    );
+  group("Get Formula1 data", () {
+    test('Get constructors data', () async {
+      List<Constructor> constructors = await getConstructors(year: 2024);
+      expect(constructors, isNotEmpty);
+      constructors.map((constructor) {
+        expect(constructor.constructorsId, isNotEmpty);
+      });
+    });
+
+    test('Get drivers data', () async {
+      List<Driver> drivers = await getDrivers(year: 2024);
+      expect(drivers, isNotEmpty);
+      drivers.map((driver) {
+        expect(driver.driverId, isNotEmpty);
+      });
+    });
+
+    test('Get results data', () async {
+      List<Race> results = await getResults(year: 2024, round: 6);
+      expect(results, isNotEmpty);
+    });
+
+    test('Get status data', () async {
+      List<Status> status = await getStatus();
+      expect(status, isNotEmpty);
+    });
   });
 }
