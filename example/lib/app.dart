f6c63b3899bbc211e3fc37ada64416fc4076593a
Heki_dm <heki.se.info@gmail.com>
2024-10-14 09:13:25 +0900

    update create example

---
+++ example/lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Formula1DataExample extends StatefulWidget {
+  const Formula1DataExample({super.key});
+
+  @override
+  State<Formula1DataExample> createState() => _Formula1DataExampleState();
+}
+
+class _Formula1DataExampleState extends State<Formula1DataExample> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
