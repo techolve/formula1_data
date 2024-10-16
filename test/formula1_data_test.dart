67e3fdeeaf81984ddba1b1d0c4d11783c095c8b0
Heki_dm <heki.se.info@gmail.com>
2024-10-16 21:29:34 +0900

    update add logger

--- test/formula1_data_test.dart
+++ test/formula1_data_test.dart
@@ -1,36 +1,42 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:formula1_data/formula1_data.dart';
 import 'package:formula1_data/src/api/drivers.dart';
 import 'package:formula1_data/src/api/results.dart';
+import 'package:formula1_data/src/api/schedules.dart';
 import 'package:formula1_data/src/api/status.dart';
+import 'package:formula1_data/src/converters/schedules/schedule.dart';
+import 'package:logger/web.dart';
 
 void main() {
+  // Setting logger instance.
+  final logger = Logger();
+
   group("Get Formula1 data", () {
+    test('Get schedule data', () async {
+      List<Schedule> schedules = await getSchedule(year: 2024);
+      logger.i(schedules);
+    });
+
     test('Get constructors data', () async {
       List<Constructor> constructors = await getConstructors(year: 2024);
-      expect(constructors, isNotEmpty);
-      constructors.map((constructor) {
-        expect(constructor.constructorsId, isNotEmpty);
-      });
+      logger.i(constructors);
     });
 
     test('Get drivers data', () async {
       List<Driver> drivers = await getDrivers(year: 2024);
-      expect(drivers, isNotEmpty);
-      drivers.map((driver) {
-        expect(driver.driverId, isNotEmpty);
-      });
+      logger.i(drivers);
     });
 
     test('Get results data', () async {
       List<Race> results = await getResults(year: 2024, round: 6);
-      expect(results, isNotEmpty);
+      logger.i(results);
     });
 
     test('Get status data', () async {
       List<Status> status = await getStatus();
-      expect(status, isNotEmpty);
+      logger.i(status);
     });
   });
 }
